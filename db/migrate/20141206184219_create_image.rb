d6bc39f2b19039fd02bd6ccc6664f9dd2726d346
alexcode18 <aschattner@gmail.com>
2014-12-06 15:23:19 -0500

    added year to image schema

--- db/migrate/20141206184219_create_image.rb
+++ db/migrate/20141206184219_create_image.rb
@@ -2,6 +2,7 @@ class CreateImage < ActiveRecord::Migration
   def change
     create_table :images do |t|
     	t.string :img_url
+    	t.integer :year
 			t.references :location
 			t.references :user
 

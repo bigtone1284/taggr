becfe3f763f6ae0f13c2fdbbd8c22691bebe2fe8
bigtone1284 <anthony.defreitas@gmail.com>
2014-12-06 15:42:32 -0500

    controllers fixed

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,11 +1,8 @@
 class Image < ActiveRecord::Base
-  validates_presence_of :first_name, :last_name, :username
-  validates_uniqueness_of :username
-  validates :first_name, :last_name, :numericality => false 
-  has_secure_password
 
-  belongs_to :locations, :users
-  has_many_and_belongs_to :tags
+  belongs_to :locations
+  belongs_to :users
+  has_and_belongs_to_many :tags
 
   def date
   	self.created_at.to_date.to_s(:long)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,11 @@
 class User < ActiveRecord::Base
-  validates_presence_of :first_name, :last_name, :username
   validates_uniqueness_of :username
-  validates :first_name, :last_name, :numericality => false 
 
   has_many :images
 
-  def full_name
-  	self.first_name + " " + self.last_name
-  end
+  # def full_name
+  # 	self.first_name + " " + self.last_name
+  # end
 
   def sign_up_date
   	self.created_at.to_date.to_s(:long)
